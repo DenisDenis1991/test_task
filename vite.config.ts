ad1c8dcf2b24aadbc785c266e85bddf0bbf017f1
Denis Mashutinskiy <mashutinsky.da@plinor.ru>
2024-10-04 23:48:49 +0300

    feat: add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import * as path from 'node:path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/test_task',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
